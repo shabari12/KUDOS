b2ee7ef22a35b1b8b4d6884a9318bc33ddc36716
Shabari Vignesh <113418289+shabari12@users.noreply.github.com>
2025-03-17 15:54:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,12 @@ const connectToDB = require("./db/db");
 const userRoutes = require("./routes/userRoutes");
 const spaceRoutes = require("./routes/spaceRoutes");
 const feedbackRoutes = require("./routes/feedbackRoutes");
+const corsOptions = {
+  origin: "https://kudos-nine-mauve.vercel.app", // Your frontend domain
+  credentials: true, // Allow cookies if needed
+};
 
-
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());
